0d34b5e3e1eef8f5ddee01c8f439c0bddbc9bbc6
HedderAlex <heddercontato@gmail.com>
2024-06-07 11:39:09 -0300

    m1

--- adicionaCurso.php
+++ adicionaCurso.php
@@ -2,10 +2,15 @@
     require 'conectaBanco.php';
     $nome = $_POST['nome'];
     $tipo = $_POST['tipo'];
-    $sql = "INSERT INTO curso(nome,tipo) VALUES ('$nome','$tipo')";
-    $banco->query($sql);
-    if($banco->affected_rows == 1){
-        echo "<p> Curso cadastrado com sucesso!</p>";
+    if($nome != "" and $tipo > 0 and $tipo < 5){
+        $sql = "INSERT INTO curso(nome,tipo) VALUES ('$nome','$tipo')";
+        $banco->query($sql);
+        if($banco->affected_rows == 1){
+            echo "<p> Curso cadastrado com sucesso!</p>";
+        }
+        else{
+            echo "Erro ao inserir os dados";
+        }
     }
     header('Location:index.php');
     $banco->close();
--- conectaBanco.php
+++ conectaBanco.php
@@ -1,6 +1,6 @@
 <?php
     $banco = new mysqli(
-        "localhost","root","","universidade","1234"
+        "localhost","root","","universidade","3306"
     );
     if($banco->connect_errno){
         echo "Erroa ao conectar no banco de dados";
